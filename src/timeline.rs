5400b7ae78c693cbb538d0bbf8b0508589a7dab5
Alex <alebuteler@gmail.com>
2023-10-09 18:13:50 -0300

    slot time to screen.

--- src/timeline.rs
+++ src/timeline.rs
@@ -33,7 +33,27 @@ impl Timeline {
 
 impl Render for Timeline {
     fn render (&self, ui: &mut Ui) {
+        // consider using ui.add_sized() for fixed widget size.
         ui.image(egui::include_image!("../assets/SpectrumBg.png"));
+
+        ui.add_space(5.0);
+        // consider simplifying to ui.horizontal
+        ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui: &mut Ui| {
+
+            for slot in &self.time_slots {
+                // probably best if I just implement a Render trait for the HourComponent struct as well.
+                // ui.add(); Rect ?
+                if slot.current {
+                    ui.strong(slot.time.to_string());
+                } else {
+                    ui.label(slot.time.to_string());
+                }
+                // padding-right
+                ui.add_space(5.0);
+            }
+        });
+        ui.add_space(5.0);
+        ui.label("Footer?");
     }
 }
     
